34b59d0e2603ae578dc6d623e9f0046c76cc4ca8
YeongSeok Kim <youngstone89@icloud.com>
2022-07-11 11:25:51 +0900

    styling

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,44 @@
 import 'package:flutter/material.dart';
 
+import './question.dart';
+
 void main() {
   runApp(MyApp());
 }
 
-class MyApp extends StatelessWidget {
+class MyApp extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _MyAppState();
+  }
+}
+
+class _MyAppState extends State<MyApp> {
+  var _questionIndex = 0;
+  void _answerQuestion() {
+    setState(() {
+      _questionIndex = _questionIndex + 1;
+    });
+  }
+
+  @override
   Widget build(BuildContext context) {
+    var questions = [
+      'what\'s your favorite color?',
+      'what\'s your favorite animal?',
+    ];
     return MaterialApp(
-      home: Text('Hello World!'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('My First App'),
+        ),
+        body: Column(children: [
+          Question(questions[_questionIndex]),
+          RaisedButton(child: Text('Answer 1'), onPressed: _answerQuestion),
+          RaisedButton(child: Text('Answer 2'), onPressed: _answerQuestion),
+          RaisedButton(child: Text('Answer 3'), onPressed: _answerQuestion),
+        ]),
+      ),
     );
   }
 }
