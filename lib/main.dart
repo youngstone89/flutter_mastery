bcf15aaf2be2c95e0699b7636c94443ec5bb5df1
youngstone89 <youngstone89@icloud.com>
2025-02-05 21:30:33 +0900

    simple button counter

--- lib/main.dart
+++ lib/main.dart
@@ -1,79 +1,44 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_mastery/quiz.dart';
-import 'package:flutter_mastery/result.dart';
 
-import './question.dart';
-import './answer.dart';
+void main() => runApp(MyApp());
 
-void main() {
-  runApp(MyApp());
-}
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-class MyApp extends StatefulWidget {
   @override
-  State<StatefulWidget> createState() {
-    return _MyAppState();
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home: Counter(),
+    );
   }
 }
 
-class _MyAppState extends State<MyApp> {
-  final _questions = const [
-    {
-      'questionText': 'what\'s your favorite color?',
-      'answers': [
-        {'text': 'Black', 'score': 1},
-        {'text': 'Red', 'score': 2},
-        {'text': 'Green', 'score': 3},
-        {'text': 'White', 'score': 5},
-      ],
-    },
-    {
-      'questionText': 'what\'s your favorite animal?',
-      'answers': [
-        {'text': 'Rabbit', 'score': 1},
-        {'text': 'Snake', 'score': 5},
-        {'text': 'Elephant', 'score': 2},
-        {'text': 'RaLionbbit', 'score': 7},
-      ],
-    },
-    {
-      'questionText': 'what\'s your favorite instructor?',
-      'answers': [
-        {'text': 'max', 'score': 1},
-        {'text': 'bob', 'score': 5},
-        {'text': 'Nil', 'score': 2},
-        {'text': 'Duke', 'score': 7},
-      ],
-    }
-  ];
-  var _questionIndex = 0;
-  var _totalScore = 0;
-  void _answerQuestion(int score) {
-    _totalScore += score;
-    setState(() {
-      _questionIndex = _questionIndex + 1;
-    });
-    if (_questionIndex < _questions.length) {
-      print('We have more questions');
-    } else {
-      print('no more questions');
-    }
+class Counter extends StatefulWidget {
+  const Counter({super.key});
+
+  @override
+  State<Counter> createState() => _CounterState();
+}
+
+class _CounterState extends State<Counter> {
+  int _count = 0;
+
+  void increaseCount() {
+    _count++;
+    print('increased count: $_count');
   }
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: Text('My First App'),
-        ),
-        body: _questionIndex < _questions.length
-            ? Quiz(
-                answerQuestion: _answerQuestion,
-                questions: _questions,
-                questionIndex: _questionIndex,
-              )
-            : Result(_totalScore),
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Sample Code'),
+      ),
+      body: Center(child: Text('You have pressed the button $_count times.')),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => setState(increaseCount),
+        tooltip: 'Increment Counter',
+        child: const Icon(Icons.add),
       ),
     );
   }
